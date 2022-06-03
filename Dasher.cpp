d6ad1885e5510624029c36ca622a1afd118be414
Trevor Braaten <80365363+TrevorBraaten@users.noreply.github.com>
2022-06-03 00:45:08 -0700

    Initial Commit

--- Dasher.cpp
+++ Dasher.cpp
@@ -15,6 +15,17 @@ int main()
   // Acceleration due to gravity (pixels/frame)/frame
     const int gravity{1'000};
 
+// Nebula Variables
+Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+Rectangle nebRec{0.0, 0.0, nebula.width/8, nebula.height/8};
+Vector2 nebPos{windowWidth, windowHeight -nebRec.height};
+
+// nebula x velocity (pixels/second)
+int nebVel{-600};
+
+
+
+// Scarfy Variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width/6;
@@ -24,6 +35,14 @@ int main()
     Vector2 scarfyPos;
     scarfyPos.x = windowWidth/2 - scarfyRec.width/2;
     scarfyPos.y = windowHeight - scarfyRec.height;
+
+
+  // Animation Frame
+  int frame{};
+  // Update Scarfys Animationn 1
+  const float updateTime {1.0/12.0};
+  // Update Animation Frame 
+  float runningTime{0};
 
 
     int velocity{0};
@@ -62,9 +81,36 @@ int main()
       {
         velocity += jumpVel;
       }
-        // Update Position
+
+      // Update Nebula Position
+      nebPos.x += nebVel * dT;
+
+
+        // Update Scarfy Position
         scarfyPos.y += velocity * dT;
 
+      // Update the running time
+        runningTime += dT;
+
+        if (runningTime >= updateTime)
+        {
+          runningTime = 0;
+
+             //Update Animation Frame
+        scarfyRec.x = frame * scarfyRec.width;
+        frame++;
+        if (frame > 5)
+        {
+          frame = 0;
+        }
+
+        }
+
+
+        // Draw Nebula
+        DrawTextureRec(nebula, nebRec, nebPos, WHITE);
+
+      // Draww Scarfy
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
 
@@ -73,5 +119,6 @@ int main()
     }
 
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
